3d0292aaa67a637255320be4ddcce52d63b50db8
Maksim Zhuk <maksim.zhuk.@phystech.edu>
2023-03-30 10:20:24 +0300

    Ip validation added

--- src/main/java/com/example/asyncparser/controller/UserController.java
+++ src/main/java/com/example/asyncparser/controller/UserController.java
@@ -5,11 +5,12 @@ import com.example.asyncparser.dto.UserResponseDTO;
 import com.example.asyncparser.service.UserGeoService;
 import com.example.asyncparser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
@@ -21,6 +22,22 @@ public class UserController {
     @Autowired
     private UserGeoService userGeoService;
 
+    public static boolean ipIsValid(String ip) {
+        String[] split = ip.split("[.]");
+        if (split.length != 4) return false;
+        try {
+            for (String s : split) {
+                int value = Integer.parseInt(s);
+                if (value < 0 || value > 255) {
+                    return false;
+                }
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
     @PostMapping("/users")
     public List<UserResponseDTO> getUsers(@RequestBody String data) {
         List<String> names = Arrays.stream(data.split("[\\n\\r]+")) //Remove all possible \r\n characters
@@ -28,12 +45,16 @@ public class UserController {
                 .collect(Collectors.toList());
         return userService.getUsers(names);
     }
-    @GetMapping("/2ip")
-    public UserGeoDataDTO getUserGeoData(@RequestParam(required = false) String ip){
-        if (ip == null)
-            ip="";
 
-        System.out.println(ip);
+    @GetMapping("/2ip")
+    public UserGeoDataDTO getUserGeoData(@RequestParam(required = false) String ip) {
+        if (ip == null) {
+            ip = "";
+        } else {
+            if (!ipIsValid(ip)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Ip=%s is invalid", ip));
+            }
+        }
         return userGeoService.getUserGeoData(ip);
     }
 }
